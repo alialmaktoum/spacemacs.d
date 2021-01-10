e8805be6e42f19dc5d642145f62e89582e6b3bf1
Ali Almaktoum <ali.almaktoum@gmail.com>
2021-01-10 10:07:17 +1000

    added dracula

--- init.el
+++ init.el
@@ -386,7 +386,8 @@ It should only modify the values of Spacemacs settings."
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
 
-   dotspacemacs-themes '(zenburn
+   dotspacemacs-themes '(dracula
+                         zenburn
                          doom-gruvbox-light
                          doom-solarized-light
                          doom-sourcerer
